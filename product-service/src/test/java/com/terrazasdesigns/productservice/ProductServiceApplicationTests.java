89886d779e8b8df6e2c4d959377070c326760946
Sam Terrazas <thechaoselement@gmail.com>
2024-01-30 19:34:56 -0700

    Test isn't working with docker but uploading for posterity

--- product-service/src/test/java/com/terrazasdesigns/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/terrazasdesigns/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.terrazasdesigns.productservice;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.terrazasdesigns.productservice.dto.ProductRequest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// BUG: The test is failing because the MongoDB container is not starting
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	// This will start a MongoDB container before the tests are run
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongodb/mongodb-community-server");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	// This will set the properties for the MongoDB container dynamically
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+		dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	// Integration test to check if the application creates a product
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(productRequestString)).andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+		.name("Iphone 13")
+		.description("iphone 13")
+		.price(BigDecimal.valueOf(1200))
+		.build();
 	}
 
 }
